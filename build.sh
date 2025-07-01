7551b99211d5d0d72a0d1aedf0451fca99fa8bda
Luis Pedro Coelho <luis@luispedro.org>
2025-07-01 13:42:31 +1000

    ENH Add simple build script
    This creates the result in the dist/ directory containing both compiled
    code and HTML files.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ve
+if test -d dist; then
+  rm -rf dist.bak
+  mv dist dist.bak
+fi
+
+wasm-pack build --target web
+mkdir -p dist
+cp -r pkg/ dist/
+cp -r index.html dist/
