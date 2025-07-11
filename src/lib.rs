f87d173b0254fb329f89e86b4f7369b8212aa632
Luis Pedro Coelho <luis@luispedro.org>
2025-07-11 14:14:10 +1000

    Add Needleman-Wunsch alignment option

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,32 @@ pub fn smith_waterman_blosum62(seq1: &str, seq2: &str, gap_penalty: i32) -> JsVa
     let result = alignment::smith_waterman_blosum62_internal(seq1, seq2, gap_penalty);
     to_value(&result).unwrap()
 }
+
+#[wasm_bindgen]
+pub fn needleman_wunsch(seq1: &str, seq2: &str) -> JsValue {
+    needleman_wunsch_custom(seq1, seq2, 2, -1, -1)
+}
+
+#[wasm_bindgen]
+pub fn needleman_wunsch_custom(
+    seq1: &str,
+    seq2: &str,
+    match_score: i32,
+    mismatch_penalty: i32,
+    gap_penalty: i32,
+) -> JsValue {
+    let result = alignment::needleman_wunsch_internal(
+        seq1,
+        seq2,
+        match_score,
+        mismatch_penalty,
+        gap_penalty,
+    );
+    to_value(&result).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn needleman_wunsch_blosum62(seq1: &str, seq2: &str, gap_penalty: i32) -> JsValue {
+    let result = alignment::needleman_wunsch_blosum62_internal(seq1, seq2, gap_penalty);
+    to_value(&result).unwrap()
+}
